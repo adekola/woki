7fadcd99650f569c65ac36f8bc694f377ff59d74
adekola <adekola.m.adebayo@gmail.com>
2017-01-07 22:51:35 +0100

    Section for search results

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -155,21 +155,64 @@
       <h2>Local discovery will never be the same</h2> <br>
     </div>
 
-<div class="ui container">
-    <div class="ui vertical small form">
-        <div class="two fields">
-            <div class="field">
-                <input placeholder="Do" type="text">
-            </div>
-            <span> in </span> 
-            <div class="field">
-                <input placeholder="Where" type="text">
-            </div>
-            <div class="ui small primary button"><i class="right arrow icon"></i></div>
+  <div class="ui container">
+      <div class="ui vertical small form">
+          <div class="two fields">
+              <div class="field">
+                  <input placeholder="Do" type="text">
+              </div>
+              <span> in </span> 
+              <div class="field">
+                  <input placeholder="Where" type="text">
+              </div>
+              <div class="ui small primary button"><i class="right arrow icon"></i></div>
+          </div>
+      </div>
+  </div>
+
+  <div class="ui container">
+    <table class="ui celled table">
+    <thead>
+      <tr><th>Name</th>
+      <th>Activity</th>
+      <th>Open/Not</th>
+    </tr></thead>
+    <tbody>
+      <tr>
+        <td>Primarck</td>
+        <td>Shop</td>
+        <td>Yes</td>
+      </tr>
+      <tr>
+        <td>Netto</td>
+        <td>Groceries</td>
+        <td>Yes</td>
+      </tr>
+      <tr>
+        <td>Burger King</td>
+        <td>Hamburgers</td>
+        <td>Yes</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr><th colspan="3">
+        <div class="ui right floated pagination menu">
+          <a class="icon item">
+            <i class="left chevron icon"></i>
+          </a>
+          <a class="item">1</a>
+          <a class="item">2</a>
+          <a class="item">3</a>
+          <a class="item">4</a>
+          <a class="icon item">
+            <i class="right chevron icon"></i>
+          </a>
         </div>
-    </div>
+      </th>
+    </tr></tfoot>
+  </table>
   </div>
-  </div>
+</div>
 
   
 </div>
