dcff00731aa41c4a199e188c1829586961d545fb
adekola <adekola.m.adebayo@gmail.com>
2017-01-10 23:06:36 +0100

    Started impl of search

---
+++ app/Http/Controllers/Search.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\District;
+use App\City;
+
+class Search extends Controller
+{
+    // Gets a shop to eat $what in $place - typically a district/city - country is inferred therefrom
+    public function EatIn($what, $place)
+    {
+       //get the place from districts or city table
+       $place = City
+
+       //then get the relevant shops from shoplocation
+    }
+
+    public function BuyIn($what, $place)
+    {
+        //get the place from districts or city table
+
+       //then get the relevant shops from shoplocation
+    }
+}
