859eb12369ad4d0ca7def3885ec852b02fb21faf
adekola <adekola.m.adebayo@gmail.com>
2017-01-29 07:33:56 +0100

    Progress with the UI for data input

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,19 +25,44 @@
     <div class="container">
       <div class="row">
         <form class="form-inline" action="/addShop" method="post">
+          <fieldset>
+            <legend>Add new shop</legend>
+            <label for="Country" class="sr-only">Country</label>
+            <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="Country">
+              <option selected>Select country</option>
+              <option value="">Germany</option>
+            </select>
 
-          <label class="sr-only" for="inlineFormInput">Name</label>
-          <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Jane Doe">
+            <label for="City" class="sr-only">City</label>
+            <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="City">
+              <option selected>Select city</option>
+              <option value="">Berlin</option>
+              <option value="">Hannover</option>
+              <option value="">Hamburg</option>
+              <option value="">Cologne</option>
+            </select>
 
-          <label for="exampleSelect1">Example select</label>
-          <select class="form-control mb-2 mr-sm-2 mb-sm-0" id="country">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
+            <label class="sr-only" class="sr-only" for="district">District</label>
+            <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0 pr-sm-0" id="district" placeholder="District">
 
+            <label class="sr-only" for="shop">Shop</label>
+            <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="shop" placeholder="Shop">
+
+            <label for="do" class="sr-only">Do</label>
+            <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="do">
+              <option selected>Select activity</option>
+              <option value="1">Do</option>
+              <option value="2">Buy</option>
+            </select>
+
+            <label for="what" class="sr-only">What</label>
+            <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="what">
+              <option selected>Select thing</option>
+              <option value="1">Food</option>
+              <option value="2">Books</option>
+            </select>
+            <button type="submit" class="btn btn-primary">Add</button>
+          </fieldset>
         </form>
       </div>
 
